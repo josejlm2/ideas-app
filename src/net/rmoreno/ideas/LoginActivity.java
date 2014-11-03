2c4a97d64c6e58d9c6ce7a2346a927153ed75868
Rafa Moreno <rmoreno.cesar@gmail.com>
2014-11-03 11:24:59 -0600

    Fixed imeOptions issue, as well as overflow in EditTexts. Added commenting to LoginActivity.

--- src/net/rmoreno/ideas/LoginActivity.java
+++ src/net/rmoreno/ideas/LoginActivity.java
@@ -19,17 +19,24 @@ public class LoginActivity extends ActionBarActivity {
 	EditText mUsername, mPassword;
 	Button mLogin;
 	TextView mSignup;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//Allows us to use progress circle
 		requestWindowFeature(Window.FEATURE_PROGRESS);
 		setContentView(R.layout.activity_login);
+		
+		//hides action bar
+		getActionBar().hide();
 		
 		mUsername = (EditText) findViewById(R.id.username);
 		mPassword = (EditText) findViewById(R.id.password);
 		mLogin = (Button) findViewById(R.id.sign_in);
 		mSignup = (TextView) findViewById(R.id.sign_up);
 		
+		//goes to signup page when clicked
 		mSignup.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -39,23 +46,30 @@ public class LoginActivity extends ActionBarActivity {
 				
 			}
 		});
+		
+		//logs user in when clicked
 		mLogin.setOnClickListener(new View.OnClickListener() {
 		
 			String username = mUsername.getText().toString().trim();
 			String password = mPassword.getText().toString().trim();
-		
+
+			//checks to make sure user exists or doesn't exist to sign them in
 			@Override
 			public void onClick(View v) {
 				ParseUser.logInInBackground(username, password, new LogInCallback() {
 					
+					//if user exists, do this
 					@Override
 					public void done(ParseUser user, ParseException e) {
 						if(user != null){
 							Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+							
+							//makes sure you can't press "back" in the next activity and come back to Login page
 							intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 							intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 							startActivity(intent);
 						}
+						//if user doesn't exist, show error message
 						else{
 							AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
 							builder.setMessage(e.getMessage())
