84d24fa0db6dafec805b9b872859c0071bcf6431
Rafa Moreno <rmoreno.cesar@gmail.com>
2014-11-09 11:32:10 -0600

    Idea now gets pushed up to Parse and is saved. have title, description, and user that saved it.

--- src/net/rmoreno/ideas/AddIdeaActivity.java
+++ src/net/rmoreno/ideas/AddIdeaActivity.java
@@ -3,6 +3,7 @@ package net.rmoreno.ideas;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -36,59 +38,62 @@ public class AddIdeaActivity extends ActionBarActivity {
 		add = (Button) findViewById(R.id.add);
 		System.out.print(add);
 		add.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				setProgressBarIndeterminateVisibility(true);
-				if(desc.getText().toString().matches("")  || title.getText().toString().matches("")){
-					AlertDialog.Builder builder = new AlertDialog.Builder(AddIdeaActivity.this);
-					builder.setMessage("Missing an input field!")
-					.setPositiveButton("Ok", null)
-					.setTitle("Error Message");
-					AlertDialog dialog = builder.create();
-					dialog.show();
-				} else{					
-					ParseObject idea = new ParseObject("New Idea");
+				//check and see all info filled
+				if(desc.getText().toString().matches("")|| title.getText().toString().matches("")){
+					Toast.makeText(AddIdeaActivity.this, "Fill in Empty Forms", Toast.LENGTH_SHORT).show();					
+				}//end if that checks null fields
+				
+				else{
+					//create new idea ParseObject
+					ParseObject idea = new ParseObject("Ideas");
 					idea.put("User", ParseUser.getCurrentUser().getUsername());
 					idea.put("Title", title.getText().toString());
 					idea.put("Description", desc.getText().toString());
-					idea.saveInBackground( new SaveCallback() {
-
+					idea.saveInBackground(new SaveCallback() {
+						
 						@Override
 						public void done(ParseException e) {
-							// TODO Auto-generated method stub
-							Log.d(e.getMessage(), null, e);
 							if(e == null){
 								AlertDialog.Builder builder = new AlertDialog.Builder(AddIdeaActivity.this);
-								builder.setMessage("Idea Added")
-								.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-				
-									@Override
-									public void onClick(DialogInterface dialog, int which) {
-										// TODO Auto-generated method stub
-										Intent intent = new Intent(AddIdeaActivity.this, MainActivity.class);
-										startActivity(intent);
-				
-									}
-								} )
-								.setTitle("Success");
+								builder.setMessage("Idea Saved")
+									.setPositiveButton("Ok", new OnClickListener() {
+										
+										@Override
+										public void onClick(DialogInterface dialog, int which) {
+											Intent intent = new Intent(AddIdeaActivity.this, MainActivity.class);
+											startActivity(intent);
+											
+										}
+									})
+									.setTitle("Success!");
 								AlertDialog dialog = builder.create();
 								dialog.show();
-							}
-							
-							else {
-								setProgressBarIndeterminateVisibility(false);
+								
+							}// end e == null if
+							else{
 								AlertDialog.Builder builder = new AlertDialog.Builder(AddIdeaActivity.this);
 								builder.setMessage(e.getMessage())
 									.setPositiveButton("Ok", null)
-									.setTitle("Error saving idea");
-							}
+									.setTitle("Error");
+								
+								AlertDialog dialog = builder.create();
+								dialog.show();
+							}//end else
+								
+							
 						}
-					});					
-				}				
-			}
-		});		
-	}
+					});//end saveInBackground
+				}//end else statement
+				
+			}//end onClick
+			
+		});//end onclick listener 
+			
+			
+	}//end on create
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
